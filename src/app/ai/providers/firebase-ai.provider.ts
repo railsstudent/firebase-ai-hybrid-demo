1680e9a43a3dbba7c121402c6f929524a54ea94b
Connie Leung <cleung334@gmail.com>
2025-11-20 23:05:42 +0800

    feat: add app check

--- src/app/ai/providers/firebase-ai.provider.ts
+++ src/app/ai/providers/firebase-ai.provider.ts
@@ -1,9 +1,22 @@
 import { makeEnvironmentProviders } from '@angular/core';
 import { getAI, getGenerativeModel, GoogleAIBackend } from 'firebase/ai';
-import { initializeApp } from "firebase/app";
+import { FirebaseApp, initializeApp } from "firebase/app";
+import { initializeAppCheck, ReCaptchaEnterpriseProvider } from 'firebase/app-check';
 import firebaseConfig from '../../firebase.json';
 import { AI_MODEL } from '../constants/firebase.constant';
 import { ImageAnalysisSchema } from '../schemas/image-analysis.schema';
+
+function initAppCheckWithRecaptcha(firebaseApp: FirebaseApp) {
+  (self as any).FIREBASE_APPCHECK_DEBUG_TOKEN = firebaseConfig?.appCheckDebugToken || firebaseConfig.isFirebaseAppCheckDebugMode;
+
+  console.log((self as any).FIREBASE_APPCHECK_DEBUG_TOKEN);
+
+  // Initialize Firebase App Check
+  initializeAppCheck(firebaseApp, {
+    provider: new ReCaptchaEnterpriseProvider(firebaseConfig.recaptchaEnterpriseSiteKey),
+    isTokenAutoRefreshEnabled: true,
+  });
+}
 
 export function provideFirebaseAiLogic() {
     return makeEnvironmentProviders([
@@ -12,24 +25,38 @@ export function provideFirebaseAiLogic() {
             useFactory: () => {
                 const { model, app } = firebaseConfig
                 const firebaseApp = initializeApp(app);
+                initAppCheckWithRecaptcha(firebaseApp)
 
                 // Initialize the Gemini Developer API backend service
                 const ai = getAI(firebaseApp, { backend: new GoogleAIBackend() });
 
                 return getGenerativeModel(ai, {
-                    mode: 'prefer_on_device',
-                    inCloudParams: {
-                        model,
-                        generationConfig: {
-                            responseMimeType: 'application/json',
-                            responseSchema: ImageAnalysisSchema
-                        },
+                    model,
+                    generationConfig: {
+                      responseMimeType: 'application/json',
+                      responseSchema: ImageAnalysisSchema
                     },
-                    onDeviceParams: {
-                        promptOptions: {
-                          responseConstraint: ImageAnalysisSchema,
-                        }
-                    }
+                    // mode: 'only_in_cloud',
+                    // inCloudParams: {
+                    //     model,
+                    //     generationConfig: {
+                    //         responseMimeType: 'application/json',
+                    //         responseSchema: ImageAnalysisSchema
+                    //     },
+                    // },
+                    // onDeviceParams: {
+                    //     createOptions: {
+                    //       expectedInputs: [
+                    //         {
+                    //           type: 'text',
+                    //           languages: ['en'],
+                    //         }
+                    //       ],
+                    //     },
+                    //     promptOptions: {
+                    //       responseConstraint: ImageAnalysisSchema,
+                    //     }
+                    // }
                 });
             }
         }
