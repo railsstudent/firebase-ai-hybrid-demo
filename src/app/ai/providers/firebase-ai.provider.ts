5b59c3f2c49f10ac8d89d8488e0516b50892dca6
Connie Leung <cleung334@gmail.com>
2025-11-24 22:05:51 +0800

    feat: add google search tool

--- src/app/ai/providers/firebase-ai.provider.ts
+++ src/app/ai/providers/firebase-ai.provider.ts
@@ -40,6 +40,7 @@ export function provideFirebaseAiLogic() {
                 return getGenerativeModel(ai, {
                     model,
                     generationConfig,
+                    tools: [{ googleSearch: {} }]
                 });
             }
         }
