ce876ef973f31648cc66133aad252139190e4ce2
Connie Leung <cleung334@gmail.com>
2025-12-27 09:13:17 +0800

    feat: display error of audio generation

--- src/app/photo-panel/obscure-fact/obscure-fact.component.ts
+++ src/app/photo-panel/obscure-fact/obscure-fact.component.ts
@@ -1,11 +1,15 @@
 import { SpeechService } from '@/ai/services/speech.service';
+import { ErrorDisplayComponent } from '@/error-display/error-display.component';
 import { ChangeDetectionStrategy, Component, inject, input, OnDestroy, signal } from '@angular/core';
-import { TextToSpeechComponent } from '../text-to-speech/text-to-speech.component';
+import { TextToSpeechComponent } from './text-to-speech/text-to-speech.component';
 
 @Component({
   selector: 'app-obscure-fact',
   templateUrl: './obscure-fact.component.html',
-  imports: [TextToSpeechComponent],
+  imports: [
+    TextToSpeechComponent,
+    ErrorDisplayComponent
+  ],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ObscureFactComponent implements OnDestroy {
@@ -17,20 +21,24 @@ export class ObscureFactComponent implements OnDestroy {
   isLoadingStream = signal(false);
   audioSyncUrl = signal<string | undefined>(undefined)
   audioBlobUrl = signal<string | undefined>(undefined)
+  error = signal('');
 
   resetAudio() {
     this.audioBlobUrl.set(undefined);
     this.audioSyncUrl.set(undefined);
   }
 
+  private revokeBlobURL() {
+    const blobUrl = this.audioBlobUrl();
+    if (blobUrl) {
+      URL.revokeObjectURL(blobUrl);
+    }
+  }
+
   async generateSpeech({ isStream }: { isStream: boolean }) {
     const fact = this.interestingFact();
     if (fact) {
-      const blobUrl = this.audioBlobUrl();
-      if (blobUrl) {
-        URL.revokeObjectURL(blobUrl);
-      }
-
+      this.revokeBlobURL()
       this.resetAudio();
       if (isStream) {
         this.generateSpeechStream(fact);
@@ -47,6 +55,7 @@ export class ObscureFactComponent implements OnDestroy {
       this.audioSyncUrl.set(uri);
     } catch (e) {
       console.error(e);
+      this.error.set('Error generating audio.');
     } finally {
       this.isLoadingSync.set(false);
     }
@@ -59,15 +68,13 @@ export class ObscureFactComponent implements OnDestroy {
       this.audioBlobUrl.set(uri);
     } catch (e) {
       console.error(e);
+      this.error.set('Error streaming audio.');
     } finally {
       this.isLoadingStream.set(false);
     }
   }
 
   ngOnDestroy(): void {
-    const blobUrl = this.audioBlobUrl();
-    if (blobUrl) {
-      URL.revokeObjectURL(blobUrl);
-    }
+    this.revokeBlobURL();
   }
 }
